f4d128e11aae5ad7c85a72a9e76c23eba03f10a5
Steinente <dawid.czech1997@gmail.com>
2021-06-09 17:23:52 +0200

    optimized

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ import SBSymbol from '../SkyblockUtilities/enums/SBSymbol';
 import Utils from '../SkyblockUtilities/utils/Utils';
 import request from '../requestV2/index';
 
+const AxisAlignedBB = Java.type('net.minecraft.util.AxisAlignedBB');
 const SharedMonsterAttributes = Java.type('net.minecraft.entity.SharedMonsterAttributes');
 
 const ModuleName = 'GhostNametags';
@@ -21,56 +22,50 @@ let creeperList = [];
 checkForUpdates();
 
 register('renderWorld', () => {
-	creeperList.forEach(creeper => {
-		drawNames(creeper);
-	});
+    if (SkyblockUtilities.getLocation() !== Location.THE_MIST) return;
+	creeperList.forEach(creeper => drawName(creeper));
+});
+
+register('worldUnload', () => {
+	creeperList = [];
 });
 
 register('tick', () => {
-    if (SkyblockUtilities.getLocation() === Location.THE_MIST) {
-        let namesNew = [];
-        World.getAllEntitiesOfType(MCEntity.CREEPER.class).forEach(creeper => {
-            namesNew.push(creeper);
-        });
-        creeperList = namesNew;
-    }
+    if (SkyblockUtilities.getLocation() !== Location.THE_MIST) return;
+    const radius = 60;
+    const scanArea = new AxisAlignedBB(
+		Player.getX() + radius,
+		Player.getY() + radius >= 255 ? 255 : Player.getY() + radius,
+		Player.getZ() + radius,
+		Player.getX() - radius,
+		Player.getY() - radius <= 0 ? 0 : Player.getY() - radius,
+		Player.getZ() - radius
+	);
+    let namesNew = [];
+    World.getWorld().func_72872_a(MCEntity.CREEPER.class, scanArea).forEach(creeper => namesNew.push(creeper)); // getEntitiesWithinAABB()
+    creeperList = namesNew;
 });
 
-function drawNames(creeper) {
-    if (Utils.getDistance(Player.getX(), Player.getY(), Player.getZ(), creeper.getX(), creeper.getY(), creeper.getZ()) <= 60) {
-        const currentHealth = creeper.entity.func_110143_aJ(); // getHealth()
-        const maxHealth = creeper.entity.func_110148_a(SharedMonsterAttributes.field_111267_a).func_111125_b(); // getEntityAttribute() | maxHealth | getBaseValue()
-        const isRunic = maxHealth === 4000000;
-        let bracketsColor = Color.DARK_GRAY;
-        let lvlColor = Color.GRAY;
-        let nameColor = Color.RED;
-        let currentHealthColor = currentHealth < maxHealth / 2 ? Color.YELLOW : Color.GREEN;
-        let maxHealthColor = Color.GREEN;
-        if (isRunic) {
-            bracketsColor = Color.DARK_PURPLE;
-            lvlColor = Color.LIGHT_PURPLE;
-            nameColor = Color.DARK_PURPLE;
-            currentHealthColor = Color.LIGHT_PURPLE;
-            maxHealthColor = Color.DARK_PURPLE;
-        }
-        const name = `${bracketsColor}[${lvlColor}Lv250${bracketsColor}] ${nameColor + (isRunic ? 'Runic' : '')} Ghost ${currentHealthColor + formatHealth(currentHealth) + Color.WHITE}/${maxHealthColor + formatHealth(maxHealth) + Color.RED + SBSymbol.HEALTH}`.replace(/&/g, '§');
-        Utils.drawString(name, creeper.getX(), creeper.getY() + 2.5, creeper.getZ(), Renderer.RED, false, 0.05, false, false);
+function drawName(creeper) {
+    const entity = new Entity(creeper);
+    if (Utils.getDistance(Player.getX(), Player.getY(), Player.getZ(), entity.getX(), entity.getY(), entity.getZ()) > 60) return;
+    const currentHealth = creeper.func_110143_aJ(); // getHealth()
+    const maxHealth = creeper.func_110148_a(SharedMonsterAttributes.field_111267_a).func_111125_b(); // getEntityAttribute() | maxHealth | getBaseValue()
+    const isRunic = maxHealth === 4000000;
+    let bracketsColor = Color.DARK_GRAY;
+    let lvlColor = Color.GRAY;
+    let nameColor = Color.RED;
+    let currentHealthColor = currentHealth < maxHealth / 2 ? Color.YELLOW : Color.GREEN;
+    let maxHealthColor = Color.GREEN;
+    if (isRunic) {
+        bracketsColor = Color.DARK_PURPLE;
+        lvlColor = Color.LIGHT_PURPLE;
+        nameColor = Color.DARK_PURPLE;
+        currentHealthColor = Color.LIGHT_PURPLE;
+        maxHealthColor = Color.DARK_PURPLE;
     }
-}
-
-function formatHealth(health) {
-    if (health >= 1000000) {
-        const hp = round(health, -5);
-        const k = hp.charAt(1);
-        return hp.charAt(0) + (k != '0' ? '.' + k : '') + 'M';
-    } else if (health < 1000000) {
-        return round(health, -3).replace('000', '') + 'k';
-    }
-}
-
-function round(number, decimalPlaces) {
-    const factorOfTen = Math.pow(10, decimalPlaces);
-    return (Math.round(number * factorOfTen) / factorOfTen).toFixed();
+    const name = `${bracketsColor}[${lvlColor}Lv250${bracketsColor}] ${nameColor + (isRunic ? 'Runic ' : '')}Ghost ${currentHealthColor + Utils.transformToSuffixedNumber(currentHealth) + Color.WHITE}/${maxHealthColor + Utils.transformToSuffixedNumber(maxHealth) + Color.RED + SBSymbol.HEALTH}`.replace(/&/g, '§');
+    Utils.drawString(name, entity.getX(), entity.getY() + 2.5, entity.getZ(), 0, false, 0.05, false, false);
 }
 
 function checkForUpdates() {
